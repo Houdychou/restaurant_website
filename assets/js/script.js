1a6ec2056192626d26bc47399886861a38ea724f
Houdychou <hchouchouh@edenschool.fr>
2024-11-01 18:04:04 +0100

    slider finished, services html done and little bit of css (not responsive)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
 
 // Function to add event listeners to multiple elements
 function addEventOnElements(elements, eventType, listener) {
-    for(let i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener(eventType, listener);
     }
 }
@@ -34,9 +34,10 @@ addEventOnElements(navbarToggler, 'click', toggleNavbar);
 const header = document.querySelector('.header');
 
 let lastScrollPos = 0;
+
 function hideHeader() {
     const isScrollBottom = lastScrollPos < window.scrollY;
-    if(isScrollBottom) {
+    if (isScrollBottom) {
         header.classList.add('hide');
     } else {
         header.classList.remove('hide');
@@ -44,11 +45,70 @@ function hideHeader() {
     lastScrollPos = window.scrollY;
 }
 
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= 50) {
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 50) {
         header.classList.add('active');
         hideHeader();
     } else {
         header.classList.remove('active');
     }
-})
+});
+
+// SLIDER
+const heroSliderItems = document.querySelectorAll('[data-hero-slider-item]');
+const heroSliderPrevBtn = document.querySelector('[data-prev-btn]');
+const heroSliderNextBtn = document.querySelector('[data-next-btn]');
+
+let currentSlidePos = 0;
+let lastActiveItem = heroSliderItems[0];
+
+function updateSlider() {
+    lastActiveItem.classList.remove('active');
+    heroSliderItems[currentSlidePos].classList.add('active');
+    lastActiveItem = heroSliderItems[currentSlidePos];
+}
+
+function nextSlide() {
+    if (currentSlidePos >= heroSliderItems.length - 1) {
+        currentSlidePos = 0;
+    } else {
+        currentSlidePos++;
+    }
+    updateSlider();
+}
+
+heroSliderNextBtn.addEventListener('click', () => {
+    nextSlide();
+});
+
+function prevSlide() {
+    if (currentSlidePos <= 0) {
+        currentSlidePos = heroSliderItems.length - 1;
+    } else {
+        currentSlidePos--;
+    }
+    updateSlider();
+}
+
+heroSliderPrevBtn.addEventListener('click', () => {
+    prevSlide();
+});
+
+// AUTO-SLIDER //
+
+let autoSlideInterval;
+function autoSlide() {
+    autoSlideInterval = setInterval(() => {
+        nextSlide();
+    }, 7000);
+}
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseover', () => {
+    clearInterval(autoSlideInterval);
+});
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseout', autoSlide);
+
+window.addEventListener('load', autoSlide);
+
+
